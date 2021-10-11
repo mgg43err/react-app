eac383318735060f2520d0ca0bfdb57e5413d486
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2021-10-12 02:55:43 +0300

    feat: add  header and footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       Simple html
+      <Footer />
     </div>
   );
 };
