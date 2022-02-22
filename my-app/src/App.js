4cfbe8740d04180d3109f38182eba11657a06e8f
ap <apleskunov@bk.ru>
2022-02-22 09:30:15 +0300

    fix:img

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,41 @@
+import React from 'react';
 import './App.css';
-import Technologies from './Technologies';
-import Header from './Header';
-
 
 const  App = ()  => {
   return (
-    <div className="App">
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
 
+      <header className='header'>
+        <img
+        src='https://icon-library.com/images/instagram-icon-24x24/instagram-icon-24x24-21.jpg'
+        alt='instagram'
+      />
+      </header>
+
+      <nav className='nav'>
+        <div>
+          <a href='#s'>Profile</a>
+        </div>
+        <div>
+          <a href='#s'>Messages</a>
+        </div>
+        <div>
+          <a href='#s'>News</a>
+        </div>
+        <div>
+          <a href='#s'>Music</a>
+        </div>
+        <div>
+          <a href='#s'>Settings</a>
+        </div>
+      </nav>
+
+      <div className='content'>
+        <img src='https://images.hdqwalls.com/download/anime-hoods-neko-ears-4k-uh-2560x1080.jpg'/>
+      </div>
     </div>
   );
 }
-
 
 export default App;
 
