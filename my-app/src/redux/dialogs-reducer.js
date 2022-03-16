dc842e1d09507ff71a8962adf77973f82164870c
ap <apleskunov@bk.ru>
2022-03-16 11:30:15 +0300

    add:deep copy of state

--- my-app/src/redux/dialogs-reducer.js
+++ my-app/src/redux/dialogs-reducer.js
@@ -1,5 +1,5 @@
-const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY"
-const SEND_MESSAGE = "SEND-MESSAGE"
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
+const SEND_MESSAGE = "SEND-MESSAGE";
 
 export const sendMessageCreator = () => ({type: SEND_MESSAGE});
 export const updateNewMessageBodyCreator = body => ({
@@ -8,89 +8,90 @@ export const updateNewMessageBodyCreator = body => ({
 });
 
 let initialState = {
+	messages: [
+		{
+			id: 1,
+			message: "Hi",
+		},
+		{
+			id: 2,
+			message: "Hi, How is your IT-kamasutra?",
+		},
+		{
+			id: 3,
+			message: "Yo",
+		},
+		{
+			id: 4,
+			message: "Frontend",
+		},
+		{
+			id: 5,
+			message: "Yes",
+		},
+		{
+			id: 6,
+			message: "YESSSS",
+		},
+		{
+			id: 7,
+			message: "Kristina is best girlfriend!",
+		},
+	],
+	dialogs: [
+		{
+			id: 1,
+			name: "Bill",
+		},
+		{
+			id: 2,
+			name: "Gaby",
+		},
+		{
+			id: 3,
+			name: "Michael",
+		},
+		{
+			id: 4,
+			name: "Noah",
+		},
+		{
+			id: 5,
+			name: "Oliver",
+		},
+		{
+			id: 6,
+			name: "John",
+		},
+		{
+			id: 7,
+			name: "Kris",
+		},
+	],
+	newMessageBody: "",
+};
 
-			messages: [
-				{
-					id: 1,
-					message: "Hi",
-				},
-				{
-					id: 2,
-					message: "Hi, How is your IT-kamasutra?",
-				},
-				{
-					id: 3,
-					message: "Yo",
-				},
-				{
-					id: 4,
-					message: "Frontend",
-				},
-				{
-					id: 5,
-					message: "Yes",
-				},
-				{
-					id: 6,
-					message: "YESSSS",
-				},
-				{
-					id: 7,
-					message: "Kristina is best girlfriend!",
-				},
-			],
-			dialogs: [
-				{
-					id: 1,
-					name: "Bill",
-				},
-				{
-					id: 2,
-					name: "Gaby",
-				},
-				{
-					id: 3,
-					name: "Michael",
-				},
-				{
-					id: 4,
-					name: "Noah",
-				},
-				{
-					id: 5,
-					name: "Oliver",
-				},
-				{
-					id: 6,
-					name: "John",
-				},
-				{
-					id: 7,
-					name: "Kris",
-				},
-			],
-			newMessageBody: "",
+const dialogsReducer = (state = initialState, action) => {
 
-}
-
-
-const dialogsReducer = (state=initialState, action) => {
-
-  switch (action.type) { 
-    case UPDATE_NEW_MESSAGE_BODY:
-      state.newMessageBody = action.body;
-      return state;
-    case SEND_MESSAGE:
-      let body = state.newMessageBody;
-      state.newMessageBody = "";
-      state.messages.push({
-        id: 8,
-        message: body,
-      });
-      return state;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case UPDATE_NEW_MESSAGE_BODY:
+			return {
+				...state,
+				newMessageBody: action.body,
+			};
+		case SEND_MESSAGE:
+			let body = state.newMessageBody;
+			return {
+				...state,
+				newMessageBody: "",
+				messages: [...state.messages, {
+				id: 8,
+				message: body,
+			}],
+			};
+		default:
+			return state;
+	}
 };
 
 export default dialogsReducer;
